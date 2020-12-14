da23e6597146dce37d0e1da955210c4e150505f1
André <andrevictor50@gmail.com>
2020-12-14 19:08:07 -0300

    setting up apollo server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,52 @@
-import express from 'express';
-import routes from './routes'
+import 'reflect-metadata';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+import AppError from './errors/AppError';
+
+import './database';
+
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const app = express();
 
-app.use(routes)
+const main = async () => {
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
 
-app.listen(3333, () => {
-  console.log('Server running on port 3333!')
+  apolloServer.applyMiddleware({
+    app,
+    cors: false
+  });
+
+  app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  });
+
+  app.listen(3333, () => {
+    console.log('🚀 Server started on port 3333');
+  });
+}
+
+main().catch(error => {
+  console.log(error);
 });
